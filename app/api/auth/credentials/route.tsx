b7f8016984fb6fa0c087beaf6e885ea66d2b4bd8
Lo <Tony.Lo2@wsp.com>
2023-08-03 15:44:00 +0800

    try fix

--- app/api/auth/credentials/route.tsx
+++ app/api/auth/credentials/route.tsx
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const res = await request.json();
 
-  if (res?.username === "admin" && res?.password === "admin")
+  if (res?.username === "admin" && res?.password === "wspisgood")
     return NextResponse.json(res);
-  else NextResponse.json({ message: "error" }, { status: 400 });
+  else return NextResponse.json({ message: "error" }, { status: 400 });
 }
